a79f848ebaf4dd882ca2a137485a310e22fa08f7
Arjun P <arjunpitchanathan@gmail.com>
2020-07-24 15:36:09 +0530

    [LinearTransform] split postMultiplyBasicSet(FlatAffineConstraints) into a separate file to avoid dependency cycle

---
+++ mlir/lib/Analysis/LinearTransformFlatAffineConstraints.cpp
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ * SPDX-FileCopyrightText: 2019 Arjun Pitchanathan <arjunpitchanathan@gmail.com>
+ */
+
+#include "mlir/Analysis/AffineStructures.h"
+#include "mlir/Analysis/Presburger/LinearTransform.h"
+
+namespace mlir {
+
+FlatAffineConstraints
+LinearTransform::postMultiplyBasicSet(const FlatAffineConstraints &bs) {
+  FlatAffineConstraints result(bs.getNumDimIds());
+
+  for (unsigned i = 0; i < bs.getNumEqualities(); ++i) {
+    ArrayRef<int64_t> eq = bs.getEquality(i);
+
+    int64_t c = eq.back();
+
+    SmallVector<int64_t, 8> newEq = postMultiplyRow(eq.drop_back());
+    newEq.push_back(c);
+    result.addEquality(newEq);
+  }
+
+  for (unsigned i = 0; i < bs.getNumInequalities(); ++i) {
+    ArrayRef<int64_t> ineq = bs.getInequality(i);
+
+    int64_t c = ineq.back();
+
+    SmallVector<int64_t, 8> newIneq = postMultiplyRow(ineq.drop_back());
+    newIneq.push_back(c);
+    result.addInequality(newIneq);
+  }
+
+  // bs.simplify(); // isl does this here
+  return result;
+}
+
+} // namespace mlir
--- mlir/lib/Analysis/Presburger/LinearTransform.cpp
+++ mlir/lib/Analysis/Presburger/LinearTransform.cpp
@@ -121,34 +121,6 @@ LinearTransform::preMultiplyColumn(ArrayRef<int64_t> col) {
   return result;
 }
 
-FlatAffineConstraints
-LinearTransform::postMultiplyBasicSet(const FlatAffineConstraints &bs) {
-  FlatAffineConstraints result(bs.getNumDimIds());
-
-  for (unsigned i = 0; i < bs.getNumEqualities(); ++i) {
-    ArrayRef<int64_t> eq = bs.getEquality(i);
-
-    int64_t c = eq.back();
-
-    SmallVector<int64_t, 8> newEq = postMultiplyRow(eq.drop_back());
-    newEq.push_back(c);
-    result.addEquality(newEq);
-  }
-
-  for (unsigned i = 0; i < bs.getNumInequalities(); ++i) {
-    ArrayRef<int64_t> ineq = bs.getInequality(i);
-
-    int64_t c = ineq.back();
-
-    SmallVector<int64_t, 8> newIneq = postMultiplyRow(ineq.drop_back());
-    newIneq.push_back(c);
-    result.addInequality(newIneq);
-  }
-
-  // bs.simplify(); // isl does this here
-  return result;
-}
-
 PresburgerBasicSet
 LinearTransform::postMultiplyBasicSet(const PresburgerBasicSet &bs) {
   PresburgerBasicSet result(bs.getNumTotalDims(), 0, 0);

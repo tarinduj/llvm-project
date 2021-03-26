48f60ac7ab2a8f5b06e4c1b82051cfa0bcc367b7
Arjun P <arjunpitchanathan@gmail.com>
2021-03-26 20:04:44 +0530

    Implement transprecision with variant and try/catch

--- mlir/include/mlir/Analysis/Presburger/Set.h
+++ mlir/include/mlir/Analysis/Presburger/Set.h
@@ -2,6 +2,7 @@
 #define MLIR_ANALYSIS_PRESBURGER_SET_H
 
 #include "mlir/Analysis/Presburger/PresburgerBasicSet.h"
+#include <variant>
 
 namespace mlir {
 namespace analysis {
@@ -50,6 +51,9 @@ public:
   // bool containsPoint(const std::vector<INT> &values) const;
   llvm::Optional<SmallVector<SafeInteger<Int>, 8>> maybeGetCachedSample() const;
 
+  template <typename OInt>
+  friend class PresburgerSet;
+
 private:
   unsigned nDim;
   unsigned nSym;
@@ -71,57 +75,33 @@ using int128_t = __int128_t;
 class TransprecSet {
 public:
   TransprecSet() {};
-  TransprecSet(PresburgerSet<int16_t> set) : set16(std::move(set)), tag(16) {}
-  TransprecSet(PresburgerSet<int64_t> set) : set64(std::move(set)), tag(64) {}
-  TransprecSet(PresburgerSet<int128_t> set) : set128(std::move(set)), tag(128) {}
+  TransprecSet(PresburgerSet<int16_t> set) : setvar(std::move(set)) {}
+  TransprecSet(PresburgerSet<int64_t> set) : setvar(std::move(set)) {}
+  TransprecSet(PresburgerSet<int128_t> set) : setvar(std::move(set)) {}
+
+  void increasePrecision() {
+    if (std::holds_alternative<PresburgerSet<int16_t>>(setvar)) {
+      setvar = PresburgerSet<int64_t>(std::get<PresburgerSet<int16_t>>(setvar));
+    } else if (std::holds_alternative<PresburgerSet<int64_t>>(setvar)) {
+      setvar = PresburgerSet<int128_t>(std::get<PresburgerSet<int64_t>>(setvar));
+    } else {
+      llvm_unreachable("Cannot expand beyond 128-bit!");
+    }
+  }
 
   bool isIntegerEmpty() {
-    bool result;
-    if (tag == 16) {
-      SafeInteger<int16_t>::overflow = false;
-      result = set16.isIntegerEmpty();
-    }
-    if (tag == 64) {
-      SafeInteger<int64_t>::overflow = false;
-      result = set64.isIntegerEmpty();
-    }
-    if (tag == 128) {
-      SafeInteger<int128_t>::overflow = false;
-      result = set128.isIntegerEmpty();
-    }
-    assert(tag == 16 || tag == 64 || tag == 128);
-    return result;
+    return std::visit([this](auto &&set) {
+      try {
+        return set.isIntegerEmpty();
+      } catch (const std::overflow_error& e) {
+        increasePrecision();
+        return this->isIntegerEmpty();
+      }
+    }, setvar);
   }
 
-  ~TransprecSet() {
-    if (tag == 16)
-      set16.~PresburgerSet<int16_t>();
-    else if (tag == 64)
-      set64.~PresburgerSet<int64_t>();
-    else if (tag == 128)
-      set128.~PresburgerSet<int128_t>();
-    else
-      llvm_unreachable("unknown tag!");
-  }
-
-  TransprecSet(const TransprecSet &o) {
-    tag = o.tag;
-    if (tag == 16)
-      set16 = o.set16;
-    else if (tag == 64)
-      set64 = o.set64;
-    else if (tag == 128)
-      set128 = o.set128;
-    else
-      llvm_unreachable("unknown tag!");
-  }
 private:
-  union {
-    PresburgerSet<int16_t> set16;
-    PresburgerSet<int64_t> set64;
-    PresburgerSet<int128_t> set128;
-  };
-  uint8_t tag;
+  std::variant<PresburgerSet<int16_t>, PresburgerSet<int64_t>, PresburgerSet<int128_t>> setvar;
 };
 
 } // namespace presburger
